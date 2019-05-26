0219c21216d6495e32c0f7d8e9d2400cda5f873c
b05902062 <b05902062@ntu.edu.tw>
2019-05-26 08:01:07 -0700

    it is able to send and receive layer two packets

--- construct_packet.c
+++ construct_packet.c
@@ -1,6 +1,5 @@
 #include "construct_packet.h"
-
-void construct_udp(unsigned int proto,unsigned char hwmac[6],unsigned char dstmac[6],unsigned int *hip,unsigned int *dip,unsigned char *packet){
+void construct_dhcp(unsigned int out_port,unsigned int type,unsigned char hwmac[6],unsigned char dstmac[6],unsigned int *hip,unsigned int *dip,unsigned char *packet){
 
 
 	memset(packet,0,PACKETMAXSIZE);
@@ -19,7 +18,7 @@ void construct_udp(unsigned int proto,unsigned char hwmac[6],unsigned char dstma
 	ip->tos = 0;
 	ip->tot_len = htons(PACKETMAXSIZE-sizeof(struct ethhdr));
 	ip->frag_off = 0;
-	ip->id = htons(54321);
+	ip->id = htons(out_port);
 	ip->ttl = 64;
 	ip->protocol = (unsigned char)IPPROTO_UDP; //17
 	ip->saddr=*hip;
@@ -36,15 +35,26 @@ void construct_udp(unsigned int proto,unsigned char hwmac[6],unsigned char dstma
 	p_udp.pad=0;
 	p_udp.proto=IPPROTO_UDP;
 	p_udp.length=htons(PACKETMAXSIZE-sizeof(struct ethhdr)-sizeof(struct iphdr));
-	p_udp.udp_hdr.source=htons(0);
-	p_udp.udp_hdr.dest=htons(0);
+	p_udp.udp_hdr.source=htons(68);
+	p_udp.udp_hdr.dest=htons(67);
 	p_udp.udp_hdr.len=htons(PACKETMAXSIZE-sizeof(struct ethhdr)-sizeof(struct iphdr));
 	p_udp.udp_hdr.check=0;
 	
 
 	//Construct application layer packet.
-	
-	construct_application_packet(proto,&p_udp);
+/*	unsigned char temp[7]={0};
+	memcpy(temp,hwmac,6);
+	printf("%s",temp);
+
+
+	for(int iii=0;iii<6;iii++){	
+		printf("%x",hwmac[iii]);
+	}
+	printf("\n");
+	for(int iii=0;iii<6;iii++){	
+		printf("%x",temp[iii]);
+	}*/
+	construct_dhcp_payload(type,&p_udp,hwmac);
 
 	//Compute checksum over the entire pseudo header, including application data.
 	p_udp.udp_hdr.check=checksum(&p_udp,sizeof(struct pseudo_udp_hdr));
@@ -57,20 +67,60 @@ void construct_udp(unsigned int proto,unsigned char hwmac[6],unsigned char dstma
 	return;
 }
 
-void construct_application_packet(unsigned int proto,struct pseudo_udp_hdr* p_udp){
+void construct_dhcp_payload(unsigned int type,struct pseudo_udp_hdr* p_udp,unsigned char *hwmac){
 
-	if(proto == 67/*dhcp*/){
-		p_udp->udp_hdr.source=htons(67);
-		p_udp->udp_hdr.dest=htons(68);
+	if(type == DHCP_DISCOVER/*dhcp*/){
+		struct dhcp_header* dhcp_hdr=(struct dhcp_header*)p_udp->data;
+		dhcp_hdr->op=1;
+		dhcp_hdr->htype=1;
+		dhcp_hdr->hlen=6;
+
+		dhcp_hdr->hops=0;
+		dhcp_hdr->xid=1111;
+		dhcp_hdr->secs=0;//for lease renewal.
+		dhcp_hdr->broadcast_rag=1;
+		dhcp_hdr->client_ip=0;
+		dhcp_hdr->your_ip=0;
+		dhcp_hdr->server_ip=0;
+		dhcp_hdr->relay_ip=0;
+		memcpy(dhcp_hdr->hw_addr,hwmac,6);
+		//dhcp_hdr->serv_name;
+		//dhcp_hdr->boot_file;
+		unsigned char*cursor=dhcp_hdr->exten;
+		unsigned char magic_cookie[5]="\x63\x82\x53\x63";
+		memcpy(cursor,magic_cookie,4);
+		cursor+=4;
+
+		cursor=dhcp_add_exten(cursor,53,1,"\x01");//dhcp discover
+		cursor=dhcp_add_exten(cursor,55,10,"\x01\x79\x03\x06\x0f\x77\xfc\x5f\x2c\x2e");//subnet mask,classless static route,router,dns server,domain name,domain search,pirvate/proxy autodiscovery,LDAP,netbios over name server,netbios over node type.
+
+		cursor=dhcp_add_exten(cursor,57,2,"\x04\x00");//max dhcp size
+		unsigned char temp[8]="\x01";
+		memcpy(temp+1,hwmac,6);
+		cursor=dhcp_add_exten(cursor,61,7,temp);//ethernet ,macaddress
+		cursor=dhcp_add_exten(cursor,51,4,"\x00\x76\xa7\x00");//lease time
+		cursor=dhcp_add_exten(cursor,12,4,"\x00\x00\x00\x00");//host name
+		cursor=dhcp_add_exten(cursor,255,0,"\x00");//end
 
 	}
 	else{
-		fprintf(stderr,"Doesn't support this protocol.");
+		fprintf(stderr,"Doesn't support this type.");
 		//Do nothing.
 	}
-
+	return;
 
 }
+unsigned char*dhcp_add_exten(unsigned char*cursor,unsigned char code,unsigned char length,unsigned char* value){
+
+	memcpy(cursor++,&code,1);
+	memcpy(cursor++,&length,1);
+	memcpy(cursor,value,length);
+	cursor+=length;
+	return cursor;
+
+}
+
+
 unsigned short checksum(void* addr,int count){
            /* Compute Internet Checksum for "count" bytes
             *         beginning at location "addr".
--- construct_packet.h
+++ construct_packet.h
@@ -6,7 +6,33 @@
 #include <stdlib.h>                 
 #include <string.h>   
 #include <stdio.h>
-#define PACKETMAXSIZE 512
+
+#define PACKETMAXSIZE 1024
+#define IP_LAYER 3
+#define TRANSPORT_LAYER 4
+#define APP_LAYER 5
+#define LINK_LAYER 2
+
+#define DHCP_DISCOVER 1
+
+struct dhcp_header{		/* BOOTP packet format */
+	unsigned char op;			/* 1=request, 2=reply */
+	unsigned char htype;		/* HW address type */
+	unsigned char hlen;		/* HW address length */
+	unsigned char hops;		/* Used only by gateways */
+	unsigned int xid;		/* Transaction ID */
+	unsigned short secs;		/* Seconds since we started */
+	unsigned short broadcast_rag:1,
+		reserved:15;		/* Just what it says */
+	unsigned int client_ip;		/* Client's IP address if known */
+	unsigned int your_ip;		/* Assigned IP address */
+	unsigned int server_ip;		/* (Next, e.g. NFS) Server's IP address */
+	unsigned int relay_ip;		/* IP address of BOOTP relay */
+	unsigned char hw_addr[16];		/* Client's HW address */
+	unsigned char serv_name[64];	/* Server host name */
+	unsigned char boot_file[128];	/* Name of boot file */
+	unsigned char exten[312];		/* DHCP options / BOOTP vendor extensions */
+};
 
 //pseudo hdr for computing udp checksum
 struct pseudo_udp_hdr{  
@@ -31,3 +57,8 @@ unsigned short checksum(void* addr,int count);
 
 void copy_macaddr(unsigned char *sll_addr,unsigned char first,unsigned char second,unsigned char third,unsigned char fourth,unsigned char fifth,unsigned char sixth);
 
+void construct_dhcp(unsigned int out_port,unsigned int type,unsigned char hwmac[6],unsigned char dstmac[6],unsigned int *hip,unsigned int *dip,unsigned char *packet);
+
+void construct_dhcp_payload(unsigned int type,struct pseudo_udp_hdr* p_udp,unsigned char *hwmac);
+
+unsigned char*dhcp_add_exten(unsigned char*cursor,unsigned char code,unsigned char length,unsigned char* value);
